ad2fa4f811d8b14b9bfa501e9a8ad809e5cfa450
liu-ming-hao <liuminghaod@foxmail.com>
2020-12-19 10:40:35 +0800

    构建代码模板，修改生成策略

--- mp_code_gen/src/main/java/com/zhengqing/aigou/GenteratorCode.java
+++ mp_code_gen/src/main/java/com/zhengqing/aigou/GenteratorCode.java
@@ -14,6 +14,10 @@ import java.util.*;
 public class GenteratorCode {
 
     public static void main(String[] args) throws InterruptedException {
+
+        String []  tables = new String[]{"ORDER_WZ_PAYMENT"};
+
+
         //用来获取Mybatis-Plus.properties文件的配置信息
         final ResourceBundle rb = ResourceBundle.getBundle("gen");
         AutoGenerator mpg = new AutoGenerator();
@@ -26,6 +30,7 @@ public class GenteratorCode {
         gc.setBaseResultMap(true);// XML ResultMap
         gc.setBaseColumnList(true);// XML columList
         gc.setAuthor(rb.getString("author"));
+        gc.setSwagger2(true);//model 生成字段说明注解
         mpg.setGlobalConfig(gc);
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
@@ -36,11 +41,12 @@ public class GenteratorCode {
         dsc.setPassword(rb.getString("jdbc.pwd"));
         dsc.setUrl(rb.getString("jdbc.url"));
         mpg.setDataSource(dsc);
-        // 策略配置
+        // ********策略配置******
         StrategyConfig strategy = new StrategyConfig();
-        //strategy.setTablePrefix(new String[] { "ORDER_" });// 此处可以修改为您的表前缀
-        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略 t_user_xxx UserXxx
-        strategy.setInclude(new String[]{"ORDER_HOLDER"}); // TODO 需要生成的表
+        //strategy.setTablePrefix(new String[] { "ORDER_" });// 表前缀忽略
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略  驼峰
+        strategy.setInclude(tables); //需要生成的表
+        strategy.setEntityLombokModel(true); //使用lombok插件 不生成getter seter
         mpg.setStrategy(strategy);
         // 包配置
         PackageConfig pc = new PackageConfig();
@@ -65,22 +71,14 @@ public class GenteratorCode {
         List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
 
         // 调整 domain 生成目录演示
-        focList.add(new FileOutConfig("/templates/entity.java.vm") {
+        /*focList.add(new FileOutConfig("/templates/entity.java.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 return rb.getString("OutputDirBase")+ "/com/zhengqing/aigou/model/" + tableInfo.getEntityName() + ".java";
             }
-        });
-        // 调整 dao 生成目录演示
-        /*focList.add(new FileOutConfig("/templates/mapper.java.vm") {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                return rb.getString("OutputDirBase")+ "/com/zhengqing/aigou/dao/" + tableInfo.getEntityName() + "Dao.java";
-            }
         });*/
-
         // 调整 xml 生成目录演示
-        focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+        focList.add(new FileOutConfig("/templates/mapperdemo.xml.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 return rb.getString("OutputDirXml")+ "/mapper/" + tableInfo.getEntityName() + "Mapper.xml";
@@ -90,14 +88,17 @@ public class GenteratorCode {
         cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
 
-        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
-        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+        /*
+         *  自定义模板配置
+         *  根据自己项目的开发风格，定义自己的模板 放在、resource/templates  下
+         *  优先加载  项目resource/templates 下的模板，然后加载  maven   mybatis-plus generator  包下内置模板
+         */
         TemplateConfig tc = new TemplateConfig();
-        tc.setService("/templates/service.java.vm");
-        tc.setServiceImpl("/templates/serviceImpl.java.vm");
-        tc.setEntity(null);
-        tc.setMapper("/templates/mapper.java.vm"); // "/templates/mapper.java.vm"
-        tc.setController("/templates/controller.java.vm");
+        tc.setService("/templates/servicedemo.java.vm");
+        tc.setServiceImpl("/templates/serviceimpldemo.java.vm");
+        tc.setEntity("/templates/entity.java.vm");
+        tc.setMapper("/templates/daodemo.java.vm"); // "/templates/mapper.java.vm"
+        tc.setController("/templates/controllerdemo.java.vm");
         tc.setXml(null);
         // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
         mpg.setTemplate(tc);
